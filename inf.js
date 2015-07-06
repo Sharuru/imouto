88ad4df9a3437179c3cd3203e3988526b75f98c4
Sharuru <mave@foxmail.com>
2015-07-06 15:24:18 +0800

    Config inf.js

--- inf.js
+++ inf.js
@@ -1,17 +1,25 @@
-var INF={
-	url:'http://mouto.org/',
-	u:[
-		'默认用户名',
-		'默认用户名2'
-	],
-	c:{
-		nichijou:["日常","我是分类简介"],
-		photo:["照片","我是分类简介"],
-		feel:["情感","我是分类简介"],
-		hide:["我是隐藏的分类",null],/*无分类简介时，当前分类将不在首页NAV显示！*/
-	},
-	wb_name:null /*设置 wb_name 可以在使用多说评论时得到私信提示功能*/
+var INF = {
+    url: 'http:/localhost/',
+    u: [
+        'Sharuru',
+        'Honoka',
+        'Umi',
+        'Kotori',
+        'Nozomi',
+        'Elichika',
+        'Nico',
+        'Rin',
+        'Hanayo',
+        'Maki'
+    ],
+    c: {
+        twoDworld: ["2D World", "二次元的世界，萌萌哒。"],
+        threeDworld: ["3D World", "现实的世界，应该有一点感悟？"],
+        zishi: ["我有姿势我自豪", "看蓝猫，学蓝猫，我有姿势我自豪！"],
+        hideLoop: ["被隐藏的世界", null]      //如果分类简介为 null 则不会再首页导航显示
+    },
+    wb_name: null       //这里是有关多说的私信设置
 };
-var DS_cfg={
-	id:'你在多说的网站ID'/*此处如留空将调用自身评论*/
+var DS_cfg = {
+    id: ''      //可以填入多说的 ID，如果为空则使用系统自带的评论模块
 };
