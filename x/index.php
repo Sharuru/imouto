c3969ddb67a1d79e8cac9bc78591964e5f443a0d
Sharuru <mave@foxmail.com>
2015-07-09 19:51:34 +0800

    Remove rss support

--- x/index.php
+++ x/index.php
@@ -1,7 +1,6 @@
 <?php
 
 function err($i){
-	//exit($i);
 	header('Content-type: application/json;charset=utf-8');
 	header('Access-Control-Allow-Origin: *');
 	exit(json_encode(array('error'=>$i)));
